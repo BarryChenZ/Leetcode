ecdc65e64e788271cedc4e130df939ab361d621a
BarryChenZ <43691684+BarryChenZ@users.noreply.github.com>
2019-07-18 13:49:56 +0800

    Create P72_EditDis.cpp

---
+++ P72_EditDis.cpp
@@ -0,0 +1,30 @@
+// Levenshtein distance
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n1 = word1.size();
+        int n2 = word2.size();
+        if(n1 == 0) return n2;
+        if(n2 == 0) return n1;
+        int res[n1 + 1][n2 + 1];
+        int cost;
+        for(int i = 0; i < n2 + 1; i++){
+            res[0][i] = i;
+        }
+        for(int i = 0; i < n1 + 1; i++){
+            res[i][0] = i;
+        }
+        for(int i = 1; i < n1 + 1; i++){
+            for(int j = 1; j < n2 + 1; j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    res[i][j] = res[i - 1][j - 1];
+                }
+                else{
+                    cost = 1;
+                    res[i][j] = cost + min(res[i - 1][j - 1], min(res[i - 1][j], res[i][j - 1]));
+                }
+            }
+        }
+        return res[n1][n2];
+    }
+};
